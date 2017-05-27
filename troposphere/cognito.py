487f23ddb065da78d954ed9db97a844dd27ed684
Prasad Kulkarni <prasad.fromgroup@gmail.com>
2017-05-27 23:08:53 +0100

    adding all aws properties

--- troposphere/cognito.py
+++ troposphere/cognito.py
@@ -121,6 +121,7 @@ class UserPoolUserToGroupAttachment(AWSObject):
       'UserPoolId' : (basestring,True)
     }
 
+# ----- Properties for AWSObject IdentityPool ------
 class CognitoIdentityProvider(AWSProperty):
     # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-identitypool-cognitoidentityprovider.html
     props = {
@@ -142,4 +143,108 @@ class PushSync(AWSProperty):
     props = {
         'ApplicationArns': (list, False),
         'RoleArn': (basestring, False)
+    }
+
+# ----- Properties for AWSObject UserPool ------
+class AdminCreateUserConfig(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-admincreateuserconfig.html
+    props = {
+        'AllowAdminCreateUserOnly': (boolean, False),
+        'InviteMessageTemplate': (InviteMessageTemplate, False),
+        'UnusedAccountValidityDays': (integer, False)
+    }
+
+class DeviceConfiguration(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-deviceconfiguration.html
+    props = {
+        'ChallengeRequiredOnNewDevice': (boolean, False),
+        'DeviceOnlyRememberedOnUserPrompt': (boolean, False)
+    }
+
+class EmailConfiguration(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-emailconfiguration.html
+    props = {
+        'ReplyToEmailAddress': (basestring, False),
+        'SourceArn': (basestring, False)
+    }
+
+class LambdaConfig(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-lambdaconfig.html
+    props = {
+        'CreateAuthChallenge': (basestring, False),
+        'CustomMessage': (basestring, False),
+        'DefineAuthChallenge': (basestring, False),
+        'PostAuthentication': (basestring, False),
+        'PostConfirmation': (basestring, False),
+        'PreAuthentication': (basestring, False),
+        'PreSignUp': (basestring, False),
+        'VerifyAuthChallengeResponse': (basestring, False)
+    }
+
+class Policies(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-policies.html
+    props = {
+        'PasswordPolicy': (PasswordPolicy, False)
+    }
+
+class SchemaAttribute(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-schemaattribute.html
+    props = {
+        'AttributeDataType': (basestring, False),
+        'DeveloperOnlyAttribute': (boolean, False),
+        'Mutable': (boolean, False),
+        'Name': (basestring, False),
+        'NumberAttributeConstraints': (NumberAttributeConstraints, False),
+        'StringAttributeConstraints': (StringAttributeConstraints, False),
+        'Required': (boolean, False)
+    }
+
+class SmsConfiguration(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-smsconfiguration.html
+    props = {
+        'ExternalId': (basestring, False),
+        'SnsCallerArn': (basestring, True)
+    }
+
+# ----- Properties for AWSObject UserPoolUser ------
+class AttributeType(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpooluser-attributetype.html
+    props = {
+        'Name': (basestring, True),
+        'Value': (basestring, False)
+    }
+
+# ----- Properties for AWSProperty AdminCreateUserConfig ------
+class InviteMessageTemplate(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-admincreateuserconfig-invitemessagetemplate.html
+    props = {
+        'EmailMessage': (basestring, False),
+        'EmailSubject': (basestring, False),
+        'SMSMessage': (basestring, False)
+    }
+
+# ----- Properties for AWSProperty Policies ------
+class PasswordPolicy(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-passwordpolicy.html
+    props = {
+        'MinimumLength': (integer, False),
+        'RequireLowercase': (boolean, False),
+        'RequireNumbers': (boolean, False),
+        'RequireSymbols': (boolean, False),
+        'RequireUppercase': (boolean, False)
+    }
+
+# ----- Properties for AWSProperty Policies ------
+class NumberAttributeConstraints(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-schemaattribute-numberattributeconstraints.html
+    props = {
+        'MaxLength': (basestring, False),
+        'MinLength': (basestring, False)
+    }
+
+class StringAttributeConstraints(AWSProperty):
+    # http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-cognito-userpool-schemaattribute-stringattributeconstraints.html
+    props = {
+        'MaxLength': (basestring, False),
+        'MinLength': (basestring, False)
     }
